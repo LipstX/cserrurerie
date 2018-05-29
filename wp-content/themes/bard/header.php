e9f1c28ebb38ebd908b45b3a5866d4b40d4444e2
ciiyama <lipstx@gmail.com>
2018-05-29 19:03:39 +0200

    Modifications Clelia

---
+++ wp-content/themes/bard/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+	<!-- Preloader -->
+	<?php get_template_part( 'templates/header/preloader' ); ?>
+
+	<!-- Page Wrapper -->
+	<div id="page-wrap">
+
+		<div id="custom-header">
+			<div class="boxed-wrapper">
+				<img id="custom-logo" src="http://localhost/cserrurerie/wp-content/uploads/custom/cornouaille-serrurie-custom-logo.png" alt="Cornouaille Serrurerie">
+				<img id="custom-icon-header" src="http://localhost/cserrurerie/wp-content/uploads/custom/cornouaille-serrurie-24.png" alt="Cornouaille Serrurerie 24h sur 24">
+			</div>
+		</div>
+
+		<!-- Boxed Wrapper -->
+		<div id="page-header" <?php echo esc_attr(bard_options( 'general_header_width' )) === 'boxed' ? 'class="boxed-wrapper"': ''; ?>>
+
+		<?php
+
+		// Top Bar
+		get_template_part( 'templates/header/top', 'bar' );
+
+		// Page Header
+		get_template_part( 'templates/header/page', 'header' );
+
+		// Main Navigation
+		get_template_part( 'templates/header/main', 'navigation' );
+		
+		?>
+
+		</div><!-- .boxed-wrapper -->
+
+		<!-- Page Content -->
+		<div id="page-content">
+			
+			<?php get_template_part( 'templates/sidebars/sidebar', 'alt' ); // Sidebar Alt ?>
